240e75b813d3910b1900e5245c75301c9dd08685
deepeshsunku <deepesh.sunku@DEV-00185-DEE-S.local>
2018-01-08 23:43:35 -0800

    Card reducer added

--- dynamic-wall/src/Components/Cards/CardView.js
+++ dynamic-wall/src/Components/Cards/CardView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import $ from "jquery";
+import { connect } from 'react-redux';
+import { addCard } from '../../Actions/actions';
 import '../../Styles/Cards.css';
 
 // Components
@@ -11,51 +12,14 @@ import Header from './Rows/Header';
 class CardView extends Component {
 	constructor(props) {
 	    super(props);
-	    console.log("Fetching morning report..");
-	    $.ajax({
-	      url: "https://api.sikkasoft.com/v2/sikkanet_cards/morning%20report?request_key=a7f4a6da5b542e141a506fa389dcb2b7&practice_id=1&startdate=2018-01-03&enddate=2018-02-02",
-	      type: "GET",
-	      beforeSend: function(request) {
-	        request.setRequestHeader("X-Auth-Token", "a7f4a6da5b542e141a506fa389dcb2b7");
-	      },
-	      contentType: "application/json",
-	      }).done(function(data) {
-	        if(data) {
-	          console.log("GET success!!!", data);
-	          // this.props.dispatch(addCard(data.Card));
-	        } else {
-	          alert("We had trouble fetching your data. Please try again.")
-	          console.log("GET auth!!!", data);
-	        }
-	      }
-	    );
-
-	    this.state = {
-		  type: "TwoColumn",
-		  title: "Morning Report",
-		  timestamp: "2018-01-05:9:00:00Z",
-		  data: {
-			  rows: [{
-			    name: "Scheduled Production",
-			    value: 7230,
-			    type: "Amount"
-			  },
-			  {
-			    name: "Scheduled Appointments",
-			    value: 10,
-			    type: "Int",
-			  }]
-		  },
-		  cta: {
-		    title: "Like what you see? Try the evening report",
-		    url: "sikkasoft.com/dentalfloss" 
-		  }
-		}
+	    console.log("this props", this.props);
 	}
+
   	render() {
-  		var rowItems = this.state.data.rows.map(function(row) {
+  		var count = 0;
+  		var rowItems = this.props.rows.map(function(row) {
 	      return (
-	        <TwoColumn name={row.name} value={row.value} />
+	        <TwoColumn name={row.name} value={row.value} key={count++}/>
 	      );
 	    });
 
@@ -63,10 +27,26 @@ class CardView extends Component {
       		<div className="Card-div">
 		        <Header title={this.props.title} posterImgUrl={this.props.posterImgUrl} postedBy={this.props.postedBy} timestamp={this.props.timestamp} />
       			{rowItems}
-      			<Cta title={this.state.cta.title} url={this.state.cta.url}/>
+      			<Cta title={this.props.cta.title} url={this.props.cta.url}/>
       		</div>
     	);
   	}
 }
 
-export default CardView;
+function mapStateToProps(state) {
+  return {
+    cards: state.cards,
+    requestKey: state.requestKey
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CardView);
