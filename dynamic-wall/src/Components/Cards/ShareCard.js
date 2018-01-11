236b3f656bb45e634c39f1b3bb03a61facb562ab
Benjamin.Holland <benjamin.holland@sikkasoftware.com>
2018-01-11 00:37:25 -0800

    RadialChartCard

--- dynamic-wall/src/Components/Cards/ShareCard.js
+++ dynamic-wall/src/Components/Cards/ShareCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LinkPreview from 'react-native-link-preview';
 import Cta from './Rows/Cta';
 import Header from './Rows/Header';
 import '../../Styles/Cards.css';
@@ -20,26 +21,35 @@ const masonry = {
 class ShareCard extends Component {
 	constructor(props) {
     super(props);
+<<<<<<< Updated upstream
+	}
+=======
+    this.state = {
+      url: null,
+      title: null,
+      description: null,
+      images: []
+    };
+>>>>>>> Stashed changes
+
+    LinkPreview.getPreview(this.props.url)
+      .then(data => this.setState({url: data.url, title: data.title, description: data.description, images: data.images}));
 	}
 
-  componentDidMount() {
-    // LinkPreview.getPreview(this.props.url)
-    // .then(function(data) {
-    //   if(this.refs.myRef) {
-    //     var description = this.state.description ? this.state.description : data.description;
-    //     var imgUrl = this.state.imgUrl ? this.state.imgUrl : data.images[0];
-    //     var title = this.state.title ? this.state.title : data.title;
-    //     this.setState({url: data.url, title: title, description: description, images: data.images, imgUrl: imgUrl});
-    //   }
-    // }.bind(this));
-  }
-
 	render() {
+<<<<<<< Updated upstream
     var imgUrl = this.props.imgUrl;
     var description = this.props.description;
 
   	return (
       <div className="Card-div" ref="myRef">
+=======
+    var imgUrl = this.props.imgUrl ? this.props.imgUrl : this.state.images[0];
+    var description = this.props.description ? this.props.description : this.state.description;
+
+  	return (
+      <div className="Card-div">
+>>>>>>> Stashed changes
         <Header title={this.props.title} color={this.props.color} posterImgUrl={this.props.posterImgUrl} postedBy={this.props.postedBy} timestamp={this.props.timestamp} />
         <div className="link-details">
           <img className="link-image" src={imgUrl} />
@@ -47,7 +57,11 @@ class ShareCard extends Component {
         <div className="link-description">
           <span className="link-description-span">{description}...</span>
         </div>
+<<<<<<< Updated upstream
         <Cta title={this.props.ctaTitle} url={this.props.url} />
+=======
+        <Cta title={this.props.ctaTitle} url={this.state.url} />
+>>>>>>> Stashed changes
       </div>
   	);
 	}
